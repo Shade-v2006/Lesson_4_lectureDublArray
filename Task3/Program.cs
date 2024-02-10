f5622fcefd6477a2ff84a481563fc58dcb95fb24
Shade-v2006 <Shade-v2006@yandex.ru>
2024-02-11 02:42:52 +0700

    Задача с чтением распознованием и выводом букв

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача принять с консоли введенное число-буквенное выражение и вывести только буквы
+
+
+string GetLettersFromString(string s) // Создаем строковую функцию (котороя будет проверять на наличие букв и плюсовать их)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine()!;
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
